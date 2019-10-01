9a0e8ee2a4564cc694bbbc8376d054700586239d
Zoe <zoe.price@kinkaid.org>
2019-10-01 14:27:09 -0500

    Made two students, looped through the first four and printed them out.

--- src/ArrayRunner.java
+++ src/ArrayRunner.java
@@ -5,5 +5,21 @@ public class ArrayRunner
         Student me = new Student("Mr. Howe","December","blue");
         System.out.println(me);
 
+        Student[] myClass = new Student[12];
+
+        Student studentA = new Student("Isabelle", "April", "pink");
+        myClass[2] = studentA;
+
+        Student studentB = new Student("Lydia", "October", "purple");
+        myClass[0] = studentB;
+
+        System.out.println(myClass[2]);
+        System.out.println(myClass[0]);
+
+        for (int i=0; i<4; i++)
+        {
+            System.out.println(myClass[i]);
+        }
+
     }
 }
